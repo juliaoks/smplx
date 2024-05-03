842a8e5b4251d8ea8780967f65d4e983ea740186
juliaoks <36921832+juliaoks@users.noreply.github.com>
2024-05-03 10:10:52 +0000

    initial fix for smplx

--- transfer_model/write_obj.py
+++ transfer_model/write_obj.py
@@ -1,4 +1,5 @@
 import os.path as osp
+import os
 import argparse
 
 import numpy as np
@@ -14,12 +15,15 @@ from tqdm.auto import tqdm, trange
 
 from pathlib import Path
 
+os.environ['DISPLAY'] = ':1'
+os.environ['PYOPENGL_PLATFORM'] = 'egl'
+
 
 def main(
     model_folder,
     motion_file,
     output_folder,
-    model_type="smplh",
+    model_type="smplx",
     ext="npz",
     gender="neutral",
     plot_joints=False,
@@ -27,8 +31,10 @@ def main(
     sample_expression=True,
     num_expression_coeffs=10,
     use_face_contour=False,
+    view=False
 ):
     output_folder = Path(output_folder)
+    os.makedirs(output_folder, exist_ok=True)
     assert output_folder.exists()
 
     # open motion file
@@ -44,18 +50,14 @@ def main(
     else:
         betas = np.zeros((num_betas,))
     num_betas = len(betas)
-    # don't know where this is documented but it's from this part of amass
-    # https://github.com/nghorbani/amass/blob/master/src/amass/data/prepare_data.py#L39-L40
-    # gdr2num = {'male':-1, 'neutral':0, 'female':1}
-    # gdr2num_rev = {v:k for k,v in gdr2num.items()}
+    print(f"{num_betas=}")
+
     if "gender" in motion:
         gender = str(motion["gender"])
     else:
         gender = gender
+    print(f"{gender=}")
 
-    print(gender)
-
-    print(num_betas)
     model = smplx.create(
         model_folder,
         model_type=model_type,
@@ -69,6 +71,7 @@ def main(
 
     betas, expression = torch.tensor(betas).float(), None
     betas = betas.unsqueeze(0)[:, : model.num_betas]
+
     if "poses" in motion:
         poses = torch.tensor(motion["poses"]).float()
     elif "smpl_poses" in motion:
@@ -79,32 +82,44 @@ def main(
                 [Body.from_smpl(p.reshape(-1, 3)).as_smplh() for p in poses]
             )
         poses = torch.tensor(poses.reshape(n, -1)).float()
-    global_orient = poses[:, :3]
+
+    if "global_orient" in motion:
+        global_orient = motion["global_orient"]
+    else:
+        global_orient = poses[:, :3]
+
     if model_type == "smplh":
         body_pose = poses[:, 3:66]
         left_hand_pose = poses[:, 66:111]
         right_hand_pose = poses[:, 111:156]
+    elif model_type == "smplx":
+        body_pose = torch.tensor(motion["body_pose"]).float()
+        left_hand_pose = torch.tensor(motion["left_hand_pose"]).float()
+        right_hand_pose = torch.tensor(motion["right_hand_pose"]).float()
+        jaw_pose = torch.tensor(motion["jaw_pose"]).float()
+        leye_pose = torch.tensor(motion["leye_pose"]).float()
+        reye_pose = torch.tensor(motion["reye_pose"]).float()
     else:
-        body_pose = poses[:, 3:]
-        left_hand_pose = np.zeros((n, 3))
-        right_hand_pose = np.zeros((n, 3))
+        body_pose = poses[:, 3:66]
+        jaw_pose = poses[:, 66:69]
+        leye_pose = poses[:, 69:72]
+        reye_pose = poses[:, 72:75]
+        left_hand_pose = poses[:, 75:120]
+        right_hand_pose = poses[:, 120:]
+
     # if sample_expression:
     #     expression = torch.randn(
     #         [1, model.num_expression_coeffs], dtype=torch.float32)
 
-    # print(expression)
-    # print(betas.shape, body_pose.shape, expression.shape)
-    for pose_idx in trange(body_pose.size(0)):
+    for pose_idx in range(body_pose.shape[0]):
         pose_idx = [pose_idx]
-        # output = model(betas=betas, # expression=expression,
-        #                return_verts=True)
         output = model(
             betas=betas,
             global_orient=global_orient[pose_idx],
             body_pose=body_pose[pose_idx],
             left_hand_pose=left_hand_pose[pose_idx],
             right_hand_pose=right_hand_pose[pose_idx],
-            # expression=expression,
+            expression=expression,
             return_verts=True,
         )
         vertices = output.vertices.detach().cpu().numpy().squeeze()
@@ -119,7 +134,7 @@ def main(
         output_path = output_folder / "{0:04d}.obj".format(pose_idx[0])
         tri_mesh.export(str(output_path))
 
-        if pose_idx[0] == 0:
+        if pose_idx[0] == 0 and view:
             print("displaying first pose, exit window to continue processing")
             mesh = pyrender.Mesh.from_trimesh(tri_mesh)
 
@@ -154,7 +169,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--model-type",
-        default="smplh",
+        default="smplx",
         type=str,
         choices=["smpl", "smplh", "smplx", "mano", "flame"],
         help="The type of model to load",
